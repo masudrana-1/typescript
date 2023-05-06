eda6ca9df1a0f3acbb9e6ff66ec972e7f90a92bd
masudrana-1 <masudrana58855@gmail.com>
2023-05-06 09:42:04 +0600

    added mapped type dynamic

--- src/module-3/mapped-type.ts
+++ src/module-3/mapped-type.ts
@@ -19,3 +19,20 @@ type Volume = {
 type Area = {
     [key in keyof Volume]: string
 }
+
+// generic 
+
+
+// type Area2<T> = {
+//     [key in keyof Volume]: string
+//     [key in keyof Volume]: Volume[key]
+//     [key in keyof T]: T[key]
+// }
+
+type Area2<T> = {
+    // [key in keyof Volume]: string
+    // [key in keyof Volume]: Volume[key]
+    readonly [key in keyof T]: T[key]
+}
+
+const area3: Area2<{ name: string, age: number }> = { name: 'masud', age: 23 }
