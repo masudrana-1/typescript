cf15c02db1d4b64d52f110f3d609634c488f9592
masudrana-1 <masudrana58855@gmail.com>
2023-05-07 09:55:19 +0600

    added access modifires

---
+++ src/module-4/access-modifires.ts
@@ -0,0 +1,28 @@
+class BankAccount {
+    readonly id: number;
+    protected name: string;
+    private _balance: number;
+
+    constructor(id: number, name: string, balance: number) {
+        this.id = id;
+        this.name = name;
+        this._balance = balance
+    }
+
+    getBalance() {
+        console.log(`My current balance is ${this._balance}`)
+    }
+
+    addDeposit(amount: number) {
+        this._balance = this._balance + amount;
+    }
+}
+
+
+// class StudentAccount extends BankAccount {
+//     test() {
+//         this.
+//     }
+// }
+
+const myAccount = new BankAccount(234, 'masud', 466767)
--- src/module-4/type-guard.ts
+++ src/module-4/type-guard.ts
@@ -40,3 +40,77 @@ const adminUser1: AdminUserType = { name: 'Mr. Y', role: 'admin' };
 
 console.log(getUser(nomalUser1));
 console.log(getUser(adminUser1));
+
+
+
+// instaceof guard 
+
+class Animal1 {
+    name: string;
+    species: string;
+    constructor(name: string, species: string) {
+        this.name = name;
+        this.species = species;
+    }
+    makeSound() {
+        console.log('I am making sound')
+    }
+}
+
+class Dog1 extends Animal1 {
+    constructor(name: string, species: string) {
+        super(name, species)
+    }
+    makeBark() {
+        console.log('I am barking');
+    }
+}
+
+class Cat1 extends Animal1 {
+    constructor(name: string, species: string) {
+        super(name, species)
+    }
+    makeMeaw() {
+        console.log('I am Meawing')
+    }
+}
+
+//************************************************* */
+// avabe o kora jay 
+function isDog(animal: Animal1): animal is Dog1 {
+    return animal instanceof Dog1
+}
+function isCat(animal: Animal1): animal is Cat1 {
+    return animal instanceof Cat1
+}
+
+function getAnimal1(animal: Animal1) {
+    if (isDog(animal)) {
+        animal.makeBark()
+    }
+    else if (isCat(animal)) {
+        animal.makeMeaw()
+    }
+    else {
+        animal.makeSound()
+    }
+}
+//************************************************* */
+
+function getAnimal(animal: Animal1) {
+    if (animal instanceof Dog1) {
+        animal.makeBark()
+    }
+    else if (animal instanceof Cat1) {
+        animal.makeMeaw()
+    }
+    else {
+        animal.makeSound()
+    }
+}
+
+const animal2 = new Dog1('German', 'dog')   //instance ---> dog
+const animal3 = new Cat1('Persian', 'cat')   //instance ---> cat
+
+getAnimal(animal3);
+
