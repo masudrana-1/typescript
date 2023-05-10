c8d222791384ec140bf7c64035e4dabbf1dc0e5a
masudrana-1 <masudrana58855@gmail.com>
2023-05-10 18:13:55 +0600

    added export

---
+++ src/module-5.5/module.ts
@@ -0,0 +1,50 @@
+// export const add = (param1: number, param2: number): number => {
+//     return param1 + param2;
+// }
+
+// export const subtract = (param1: number, param2: number): number => {
+//     return param1 - param2;
+// }
+
+// export const multiply = (param1: number, param2: number): number => {
+//     return param1 * param2;
+// }
+
+// export const divide = (param1: number, param2: number): number => {
+//     return param1 / param2;
+// }
+
+// export const average = (param1: number, param2: number): number => {
+//     return (param1 + param2) / 2;
+// }
+
+const add = (param1: number, param2: number): number => {
+    return param1 + param2;
+}
+
+const subtract = (param1: number, param2: number): number => {
+    return param1 - param2;
+}
+
+const multiply = (param1: number, param2: number): number => {
+    return param1 * param2;
+}
+
+const divide = (param1: number, param2: number): number => {
+    return param1 / param2;
+}
+
+const average = (param1: number, param2: number): number => {
+    return (param1 + param2) / 2;
+}
+
+export default {
+
+    add,
+    subtract,
+    multiply,
+    divide,
+    average
+
+};
+
